280a68612224980ed562be4552427030065570b5
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-25 10:25:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,25 +11,6 @@ client.on('message', message => {
       }
 });
 
- if (msg.content === 'باك') {
-    msg.reply('**ﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**');
-  }
-});
-
- if (msg.content === 'برب') {
-    msg.reply('**تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**');
-  }
-});
-
- if (msg.content === 'رابط') {
-    msg.reply('**``https://discord.gg/EXNztT6``**');
-  }
-});
-
- if (msg.content === 'الرابط') {
-    msg.reply('**``https://discord.gg/EXNztT6``**');
-  }
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
