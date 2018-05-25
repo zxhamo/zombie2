92b7db7be1903acee15f453475eaccd7c85f05fd
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-05-25 10:20:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,26 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+ if (msg.content === 'باك') {
+    msg.reply('**ﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**');
+  }
+});
+
+ if (msg.content === 'برب') {
+    msg.reply('**تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**');
+  }
+});
+
+ if (msg.content === 'رابط') {
+    msg.reply('**``https://discord.gg/EXNztT6``**');
+  }
+});
+
+ if (msg.content === 'الرابط') {
+    msg.reply('**``https://discord.gg/EXNztT6``**');
+  }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
